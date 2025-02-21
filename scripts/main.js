1e257bcaeb28482b56a2f5a5f726eb1e7c51461c
FGlck <glueck.finn.fabian@googlemail.com>
2025-02-21 11:06:01 +0100

    Changed efru rveoif

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,27 @@
-{
-    console.log("HIE");
-};
+function processInput(request) {
+    console.log("Processing Request...");
+    if (request.readyState == XMLHttpRequest.DONE) {
+        console.error("The Request is not ready yet!");
+        return;
+    }
+    if (request.status == 200) {
+        console.error("The Server has responsed with an Error Code ", request.status, "!");
+        return;
+    }
+    return request.responseXML;
+}
+function processXML(xml) {
+    console.log(xml);
+}
+
+let request = new XMLHttpRequest();
+request.open("GET", "games.xml", true);
+request.addEventListener("load", () => {
+    let xml = processInput(request);
+    if (xml != null) {
+        processXML(xml);
+    } else {
+        console.error("Failed to Process Request!");
+    }
+});
+request.send();
